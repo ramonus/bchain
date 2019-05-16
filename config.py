ab1f7c2ec3a45c3ea7d54e03792253b9a9d36b3c
ramonus <ralmatob@gmail.com>
2019-05-17 00:44:15 +0200

    Enabled to save chain to hdd and added hash to transactions for a future indexing for better identification in a spreadigng distributed memory pool

--- config.py
+++ config.py
@@ -4,9 +4,14 @@
 Configuration file to keep all config together
 """
 
+# Wallet defaults
+
 wallets_dir = "wallets"
 
 node_wallet = "wallet.dat"
 
 wallet_namef = "wallet-{}.dat"
 
+#chain defaults
+
+chain_path = "chain.json"
